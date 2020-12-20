a46f5bc5c52e53b2f9b87a18e1df15a8b6d335f5
e1078 <emile.jaffrain@gmail.com>
2020-12-20 21:46:18 +0100

    adding icons and titles

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,7 +4,7 @@ import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
+    /* ready() {
       console.log(
         "App is being served from cache by a service worker.\n" +
           "For more details, visit https://goo.gl/AFskqB"
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === "production") {
     },
     error(error) {
       console.error("Error during service worker registration:", error);
-    }
+    } */
   });
 }
