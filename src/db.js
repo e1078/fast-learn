a647b82d53adc27083721fdd630642283cddbf87
e1078 <emile.jaffrain@gmail.com>
2020-12-21 10:00:58 +0100

    updating firestore persitence

--- src/db.js
+++ src/db.js
@@ -1,8 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-export const db = firebase
-  .initializeApp({ projectId: "fast-learn-18c35" })
-  .firestore();
+firebase.firestore().enablePersistence();
 
-db.enablePersistence();
+export const db = firebase
+  .initializeApp({
+    apiKey: "AIzaSyCGodojEQo87S4DAiUsp3GmsdxvxtuB8Qk",
+    authDomain: "fast-learn-18c35.firebaseapp.com",
+    projectId: "fast-learn-18c35",
+    storageBucket: "fast-learn-18c35.appspot.com",
+    messagingSenderId: "186458413201",
+    appId: "1:186458413201:web:13e36574ccd8cc56090730"
+  })
+  .firestore();
